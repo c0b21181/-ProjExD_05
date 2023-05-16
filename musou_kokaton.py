86d19b55a1206728db5c0da86c48f2bb3c4ef7ec
eno <c0a2202682@edu.teu.ac.jp>
2023-05-16 15:58:22 +0900

    課題1(LSHIFTで速度が二倍になります)

--- musou_kokaton.py
+++ musou_kokaton.py
@@ -70,7 +70,6 @@ class Bird(pg.sprite.Sprite):
         self.image = self.imgs[self.dire]
         self.rect = self.image.get_rect()
         self.rect.center = xy
-        self.speed = 10
 
     def change_img(self, num: int, screen: pg.Surface):
         """
@@ -270,6 +269,10 @@ def main():
                 return 0
             if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
                 beams.add(Beam(bird))
+            if event.type == pg.KEYDOWN and event.key == pg.K_LSHIFT:
+                bird.speed = 20
+            else:
+                bird.speed = 10
         screen.blit(bg_img, [0, 0])
 
         if tmr%200 == 0:  # 200フレームに1回，敵機を出現させる
@@ -297,6 +300,7 @@ def main():
             return
 
         bird.update(key_lst, screen)
+        print(bird.speed)
         beams.update()
         beams.draw(screen)
         emys.update()
